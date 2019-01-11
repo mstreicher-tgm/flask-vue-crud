a821861b5c4782ac9aeaf18cb3768dd928d21bbb
Matthias Streicher <mstreicher@student.tgm.ac.at>
2019-01-11 15:47:26 +0100

    TODO-Liste mittels Flask implementiert.

--- server/app.py
+++ server/app.py
@@ -16,28 +16,25 @@ app.config.from_object(__name__)
 # enable CORS
 CORS(app)
 
-BOOKS = [
+TODOS = [
     {
         'id': uuid.uuid4().hex,
-        'title': 'On the Road',
-        'author': 'Jack Kerouac',
-        'read': True,
-        'price': '19.99'
+        'todo': 'SEW Test verbessern.',
+        'assignee': 'Matthias Streicher',
+        'done': True,
     },
     {
         'id': uuid.uuid4().hex,
-        'title': 'Harry Potter and the Philosopher\'s Stone',
-        'author': 'J. K. Rowling',
-        'read': False,
-        'price': '9.99'
+        'todo': 'Mathe Hausübung machen.',
+        'assignee': 'David Samwald',
+        'done': False,
     },
     {
         'id': uuid.uuid4().hex,
-        'title': 'Green Eggs and Ham',
-        'author': 'Dr. Seuss',
-        'read': True,
-        'price': '3.99'
-    }
+        'todo': 'Mitschrift nach schreiben.',
+        'assignee': 'Alexander Hold',
+        'done': False,
+    },
 ]
 
 
@@ -47,83 +44,52 @@ def ping_pong():
     return jsonify('pong!')
 
 
-@app.route('/books', methods=['GET', 'POST'])
-def all_books():
+@app.route('/todos', methods=['GET', 'POST'])
+def all_tasks():
     response_object = {'status': 'success'}
     if request.method == 'POST':
         post_data = request.get_json()
-        BOOKS.append({
+        TODOS.append({
             'id': uuid.uuid4().hex,
-            'title': post_data.get('title'),
-            'author': post_data.get('author'),
-            'read': post_data.get('read'),
-            'price': post_data.get('price')
+            'todo': post_data.get('todo'),
+            'assignee': post_data.get('assignee'),
+            'done': post_data.get('done')
         })
-        response_object['message'] = 'Book added!'
+        response_object['message'] = 'Task added!'
     else:
-        response_object['books'] = BOOKS
+        response_object['todos'] = TODOS
     return jsonify(response_object)
 
 
-@app.route('/books/<book_id>', methods=['GET', 'PUT', 'DELETE'])
-def single_book(book_id):
+@app.route('/todos/<task_id>', methods=['GET', 'PUT', 'DELETE'])
+def single_task(task_id):
     response_object = {'status': 'success'}
     if request.method == 'GET':
         # TODO: refactor to a lambda and filter
-        return_book = ''
-        for book in BOOKS:
-            if book['id'] == book_id:
-                return_book = book
-        response_object['book'] = return_book
+        return_task = ''
+        for task in TODOS:
+            if task['id'] == task_id:
+                return_task = task
+        response_object['task'] = return_task
     if request.method == 'PUT':
         post_data = request.get_json()
-        remove_book(book_id)
-        BOOKS.append({
+        remove_task(task_id)
+        TODOS.append({
             'id': uuid.uuid4().hex,
-            'title': post_data.get('title'),
-            'author': post_data.get('author'),
-            'read': post_data.get('read'),
-            'price': post_data.get('price')
+            'todo': post_data.get('todo'),
+            'assignee': post_data.get('assignee'),
+            'done': post_data.get('done')
         })
-        response_object['message'] = 'Book updated!'
+        response_object['message'] = 'Task updated!'
     if request.method == 'DELETE':
-        remove_book(book_id)
-        response_object['message'] = 'Book removed!'
+        remove_task(task_id)
+        response_object['message'] = 'Task removed!'
     return jsonify(response_object)
 
-
-@app.route('/charge', methods=['POST'])
-def create_charge():
-    post_data = request.get_json()
-    amount = round(float(post_data.get('book')['price']) * 100)
-    stripe.api_key = os.environ.get('STRIPE_SECRET_KEY')
-    charge = stripe.Charge.create(
-        amount=amount,
-        currency='usd',
-        card=post_data.get('token'),
-        description=post_data.get('book')['title']
-    )
-    response_object = {
-        'status': 'success',
-        'charge': charge
-    }
-    return jsonify(response_object), 200
-
-
-@app.route('/charge/<charge_id>')
-def get_charge(charge_id):
-    stripe.api_key = os.environ.get('STRIPE_SECRET_KEY')
-    response_object = {
-        'status': 'success',
-        'charge': stripe.Charge.retrieve(charge_id)
-    }
-    return jsonify(response_object), 200
-
-
-def remove_book(book_id):
-    for book in BOOKS:
-        if book['id'] == book_id:
-            BOOKS.remove(book)
+def remove_task(task_id):
+    for task in TODOS:
+        if task['id'] == task_id:
+            TODOS.remove(task)
             return True
     return False
 
